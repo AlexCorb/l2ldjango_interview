be02393f26bef21a922c19d67bb93e79cf1db22a
AlexCorb <ajcorbeill@gmail.com>
2023-11-21 15:49:59 -0600

    Removed extraneous code

--- l2l/templatetags/l2l_extras.py
+++ l2l/templatetags/l2l_extras.py
@@ -1,13 +1,11 @@
 from django import template
 import datetime
-import re
 
 register = template.Library()
 
 @register.filter(name="l2l_dt")
 def l2l_dt(value) :
     """Custom Date Filter"""
-    #return type(value)
     if isinstance(value, datetime.datetime):
         return value
     else:
